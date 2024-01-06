d5c20219d5f441bec7349203ce3ead5b2f710442
Carolyn Whelpley <33427006+mcdonaldcarolyn@users.noreply.github.com>
2024-01-06 14:08:10 +0000

    create iniital input

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 import sys
 
 print (" Fibonacci sequence by inventwithpython.com")
+
+while True:
+    while True:
+        print('Enter the Nth Fibonacci number you wish to')
+        print('calculate (such as 5, 50, 100), or QUIT to quit:')
+        response = input('> ').upper()
