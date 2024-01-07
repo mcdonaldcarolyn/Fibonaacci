2091151db026161248ea6e9fb4899e5695c34647
Carolyn Whelpley <33427006+mcdonaldcarolyn@users.noreply.github.com>
2024-01-07 18:29:07 +0000

    add input for enter number

--- main.py
+++ main.py
@@ -11,3 +11,15 @@ while True:
         if response == 'QUIT':
             print('Thanks for playing')
             sys.exit()
+        if response.isdecimal() and int(respopnse) != 0:
+            nth = int(response)
+            break
+        print('please enter a number bigger than 0 or QUIT')
+    print()
+
+    if nth == 1:
+        print('0')
+        print()
+        print('the #1 Fibonacci number is 0')
+        continue
+    
